6cea4027d3f3f3d3e8738857689663e9fdd79e2b
Miltos Konstantinidis <153661064+eldonate@users.noreply.github.com>
2024-05-15 01:26:09 +0300

    Update get_events.php to include config.php

--- events/get_events.php
+++ events/get_events.php
@@ -1,10 +1,10 @@
 <?php
 // Database connection
-$servername = "localhost";
-$username = "events_user";
-$password = "Uad0cAm5d008_8_7d8";
-$dbname = "events";
-
+    include_once 'config.php';
+    $servername = DB_HOST;
+    $username = DB_USER;
+    $password = DB_PASSWORD;
+    $dbname = DB_NAME;
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
