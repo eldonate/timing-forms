749e2be4bb268de3ba0aeb6c34f013107d153bd1
Miltos Konstantinidis <153661064+eldonate@users.noreply.github.com>
2024-05-15 01:14:41 +0300

    Update process_form.php to test config.php

--- events/process_form.php
+++ events/process_form.php
@@ -1,9 +1,10 @@
 <?php
 // DB credentials
-$servername = "localhost";
-$username = "events_user";
-$password = "Uad0cAm5d008_8_7d8";
-$dbname = "events";
+include_once '/etc/config/config.php';
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname =  getenv('DB_NAME');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
