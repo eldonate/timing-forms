1f4171fb714175bce04e6559e153af5b0aee534a
Miltos Konstantinidis <153661064+eldonate@users.noreply.github.com>
2024-05-17 01:03:07 +0300

    Update config_sample.php

--- events/config_sample.php
+++ events/config_sample.php
@@ -4,4 +4,10 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'user');
 define('DB_PASSWORD', 'password');
 define('DB_NAME', 'database');
+
+// SMTP configuration
+define('SMTP_HOST', 'your_smtp_host');
+define('SMTP_USERNAME', 'your_smtp_username');
+define('SMTP_PASSWORD', 'your_smtp_password');
+define('SMTP_PORT', 587); // Update with your SMTP port
 ?>
