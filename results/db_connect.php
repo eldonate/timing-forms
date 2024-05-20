93b8fc3b39740e5aa3cc766ccce0d70f79bacf18
Miltos Konstantinidis <153661064+eldonate@users.noreply.github.com>
2024-05-20 16:49:21 +0300

    Create db_connect.php

---
+++ results/db_connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'dbname';
+$username = 'dbusername';
+$password = 'password'; // **Keep this confidential!**
+
+function connect_to_db() {
+  global $host, $dbname, $username, $password;
+
+  $conn = mysqli_connect($host, $username, $password, $dbname);
+
+  if (!$conn) {
+    echo "Error: Could not connect to database. " . mysqli_connect_error();
+    exit;
+  }
+
+  return $conn;
+}
