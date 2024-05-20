553a75ad02cd68f39380629fceabfcf3f49e7ad6
Miltos Konstantinidis <153661064+eldonate@users.noreply.github.com>
2024-05-20 16:52:08 +0300

    Update db_connect.php

--- results/db_connect.php
+++ results/db_connect.php
@@ -1,9 +1,9 @@
 <?php
-
-$host = 'localhost';
-$dbname = 'dbname';
-$username = 'dbusername';
-$password = 'password';
+include_once 'api_config.php';
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASSWORD;
+$dbname = DB_NAME;
 
 function connect_to_db() {
   global $host, $dbname, $username, $password;
