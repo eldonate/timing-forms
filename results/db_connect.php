9f4b83f0f85bef8558014b91dbdefef70e54707a
Miltos Konstantinidis <153661064+eldonate@users.noreply.github.com>
2024-05-25 01:27:49 +0300

    Update db_connect.php

--- results/db_connect.php
+++ results/db_connect.php
@@ -1,19 +1,21 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'testapi';
-$username = 'testapi';
-$password = '2iu25?Xn2';
+include 'api_config.php';
 
 function connect_to_db() {
-  global $host, $dbname, $username, $password;
+    global $db_config;
 
-  $conn = mysqli_connect($host, $username, $password, $dbname);
+    $conn = mysqli_connect(
+        $db_config['host'], 
+        $db_config['username'], 
+        $db_config['password'], 
+        $db_config['dbname']
+    );
 
-  if (!$conn) {
-    echo "Error: Could not connect to database. " . mysqli_connect_error();
-    exit;
-  }
+    if (!$conn) {
+        echo "Error: Could not connect to database. " . mysqli_connect_error();
+        exit;
+    }
 
-  return $conn;
+    return $conn;
 }
